7dffae3e74b051dd11cf713b4c10ee6c3a51900b
renny <1258653+rennyv@users.noreply.github.com>
2021-09-04 19:26:26 -0600

    added q116 and q10617

---
+++ q0617.merge.two.binary.trees/py3/main.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        head = None
+        if root1 and root2:
+            head = TreeNode(root1.val+root2.val, self.mergeTrees(root1.left, root2.left), self.mergeTrees(root1.right, root2.right))
+        elif root1 and root2 is None:
+            head = TreeNode(root1.val, self.mergeTrees(root1.left, root2), self.mergeTrees(root1.right, root2))
+        elif root1 is None and root2:
+            head = TreeNode(root2.val, self.mergeTrees(root1, root2.left), self.mergeTrees(root1, root2.right))
+        return head
+
+    
+        # Solution from site
+        # if (t1 == null):
+        #     return t2;
+        # if (t2 == null):
+        #     return t1;
+        # t1.val += t2.val;
+        # t1.left = mergeTrees(t1.left, t2.left);
+        # t1.right = mergeTrees(t1.right, t2.right);
+        # return t1;
