d06ed05869c6676587cd26249ee089ee2e6fc632
renny <1258653+rennyv@users.noreply.github.com>
2021-11-03 19:39:36 -0600

    added q0404

---
+++ q0404.sum.of.left.leaves/py3/main.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        if root is None or (root.left is None and root.right is None): return 0
+        
+        def sumleft(root, left):
+            if root is None: return 0
+            
+            if root.left is None and root.right is None and left:
+                return root.val
+              
+            return sumleft(root.left, True) + sumleft(root.right, False)
+
+        return sumleft(root, False)
