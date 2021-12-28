3bd92fc6a7ddefc612c7b593f67df1e9f2cea7e6
renny <1258653+rennyv@users.noreply.github.com>
2021-12-28 09:34:46 -0700

    updated code

--- q0876.middle.of.the.linked.list/py3/main.py
+++ q0876.middle.of.the.linked.list/py3/main.py
@@ -5,12 +5,19 @@
 #         self.next = next
 class Solution:
      def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        midNode=head
-        moveNode=head
-        move=False
-        while not (moveNode is None):
-            moveNode = moveNode.next
-            if move:
-                midNode=midNode.next
-            move= not move
-        return midNode
+        # midNode=head
+        # moveNode=head
+        # move=False
+        # while not (moveNode is None):
+        #     moveNode = moveNode.next
+        #     if move:
+        #         midNode=midNode.next
+        #     move= not move
+        # return midNode
+
+        #try a couple months later
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
